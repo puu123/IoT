8cd5520e25234f3ee80da40f20a195a30b82ceff
motohashi <motohashi@seamark.co.jp>
2017-01-19 20:53:51 +0900

    ピンを２に

--- pi4j-Lchika/src/main/java/puu123/ControlGpioExample.java
+++ pi4j-Lchika/src/main/java/puu123/ControlGpioExample.java
@@ -22,7 +22,7 @@ public class ControlGpioExample {
         final GpioController gpio = GpioFactory.getInstance();
 
         // provision gpio pin #01 as an output pin and turn on
-        final GpioPinDigitalOutput pin = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_01, "MyLED", PinState.HIGH);
+        final GpioPinDigitalOutput pin = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_02, "MyLED", PinState.HIGH);
         System.out.println("--> GPIO state should be: ON");
 
         Thread.sleep(5000);
